f52359bfa04940b31f2c5cb2b20b0faaac4b5262
Noah James Forrest <noah.forrest@northwestern.edu>
2022-05-14 20:43:37 -0500

    Simulated Diabetes Data and App Skeleton

--- app.R
+++ app.R
@@ -3,26 +3,44 @@
 # from Noah: will make simulated data this week. 
 
 library(shiny)
+library(tidyverse)
+diabetes = read_csv("diabetes_dataset.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Provider Dashboard"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          #only diabetes works right now
+          selectInput("dz", 
+                      "What disease process?", 
+                      choices = list("Diabetes", 
+                                     "Heart Failure")), 
+          conditionalPanel(
+            condition = "input.dz == 'Diabetes'",
+            selectInput("diabetes_drop", 
+                        "Which diabetes-related condition/lab?", 
+                        choices = as.list(names(diabetes)[!str_detect(names(diabetes), "id$")])
+            ), 
+            conditionalPanel(
+              condition = "input.dz == 'Diabetes'", 
+              radioButtons('due_filters', 
+                           "Label Plot by Vital Due Dates", 
+                           as.list(names(diabetes)[str_detect(names(diabetes), "due")]))
+              
+              
+            )
+          )
+            
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("condition_plot")
         )
     )
 )
@@ -30,13 +48,33 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$condition_plot <- renderPlot({
+      if(input$dz == "Diabetes") { 
+        if(class(diabetes %>% pull(input$diabetes_drop)) == "character") { 
+          #categorical variables
+          temp = diabetes %>% 
+            group_by(across(all_of(c(input$diabetes_drop, input$due_filters)))) %>%  
+            count()
+          
+          
+          ggplot(temp, aes_string(input$diabetes_drop, "n", 
+                                  fill = input$due_filters))+
+            geom_col(color = "black")+
+            theme_bw()+
+            labs(x = "Values", 
+                 y = "N", 
+                 title = input$diabetes_drop)
+        } else{ 
+          ggplot(diabetes, aes_string(input$diabetes_drop, fill = input$due_filters))+
+            geom_histogram(color = "black")+
+            theme_bw()+
+            labs(title = paste0(input$diabetes_drop))
+        }
+        
+        
+      }
+      
+       
     })
 }
 
