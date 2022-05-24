e0d93411d8ad672eab0c4bbdcbee2ba0b2b48633
Noah James Forrest <noah.forrest@northwestern.edu>
2022-05-24 11:53:31 -0500

    Incorporated Diabetes Time Data and Hospital Readmissions into App

--- app.R
+++ app.R
@@ -4,13 +4,29 @@
 
 library(shiny)
 library(tidyverse)
+library(lubridate)
 diabetes = read_csv("diabetes_dataset.csv")
+diabetes_time = read_csv("a1c_time_series.csv") %>%  
+  mutate(hba1c_normal = paste0("Last HbA1c ", hba1c_normal))
+readmit = read_csv("readmission.csv") %>%  
+  rename(number_of_readmissions = readmit_number)
+
+
+readmit_variables = as.list(names(readmit)[!str_detect(names(readmit), "id")])
+names(readmit_variables) = str_to_title(str_replace_all(readmit_variables, 
+                                                        "_", 
+                                                        " "))
+diabetes_variables = as.list(
+  c(names(diabetes)[!str_detect(names(diabetes), "id$")], 
+    "a1c_over_time"))
+#displays as capitalized and wihtout underscore
+names(diabetes_variables) = str_to_title(str_replace_all(diabetes_variables, "_", " "))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Provider Dashboard"),
+    titlePanel("Report Visualization"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -19,21 +35,60 @@ ui <- fluidPage(
           selectInput("dz", 
                       "What disease process?", 
                       choices = list("Diabetes", 
-                                     "Heart Failure")), 
+                                     "Hospital Readmission")), 
           conditionalPanel(
             condition = "input.dz == 'Diabetes'",
             selectInput("diabetes_drop", 
                         "Which diabetes-related condition/lab?", 
-                        choices = as.list(names(diabetes)[!str_detect(names(diabetes), "id$")])
+                        choices = diabetes_variables
             ), 
             conditionalPanel(
-              condition = "input.dz == 'Diabetes'", 
+              condition = "input.dz == 'Diabetes' && input.diabetes_drop != 'a1c_over_time'", 
               radioButtons('due_filters', 
                            "Label Plot by Vital Due Dates", 
-                           as.list(names(diabetes)[str_detect(names(diabetes), "due")]))
-              
-              
+                           choiceValues = as.list(names(diabetes)[str_detect(names(diabetes), "due")]), 
+                           choiceNames = as.list(
+                             str_to_title(str_replace_all(names(diabetes)[str_detect(names(diabetes), "due")], 
+                                             "_", 
+                                             " "))
+                             )
+                           )
             )
+          ), 
+          conditionalPanel(
+            condition = "input.dz == 'Diabetes' && input.diabetes_drop == 'a1c_over_time'",
+            radioButtons('time_filter', 
+                         "Pick a Time Period to Filter By", 
+                         choiceValues = list(as.character(min(diabetes_time$test_date)), 
+                                        as.character(Sys.Date() - months(6)), 
+                                        as.character(Sys.Date() - years(1))), 
+                         choiceNames = list("All Available Data", 
+                                            "Last 6 Months of Data", 
+                                            "Last Year of Data")
+              
+            ), 
+            radioButtons('patient_filter', 
+                         "What Patient Population Do You Want to Look At?", 
+                         choiceNames = list("All Patients", 
+                                            "Last HbA1c in Top 25%", 
+                                            "Last HbA1c in Top 50%"), 
+                         choiceValues = list(1, 4, 2))
+          ), 
+          conditionalPanel(
+            condition = "input.dz != 'Diabetes'",
+            selectInput("readmit_drop", 
+                        label = "Which readmit variable do you want to visualize?", 
+                        choices = readmit_variables), 
+            radioButtons('readmit_filter', 
+                         "What time period?", 
+                         choiceNames = list("All Available Data", 
+                                            "Last 6 Months of Data", 
+                                            "Last 3 Months of Data"), 
+                         choiceValues = list(as.character(min(readmit$last_readmit)), 
+                                             as.character(Sys.Date() - months(6)), 
+                                             as.character(Sys.Date() - months(3)))
+                         )
+            
           )
             
         ),
@@ -50,7 +105,26 @@ server <- function(input, output) {
 
     output$condition_plot <- renderPlot({
       if(input$dz == "Diabetes") { 
-        if(class(diabetes %>% pull(input$diabetes_drop)) == "character") { 
+       
+        if(input$diabetes_drop == "a1c_over_time") {
+          recent_data = diabetes %>%  
+            mutate(quartile = ntile(hba1c_value, 4)) %>% 
+            filter(quartile >= as.numeric(input$patient_filter))
+          
+          temp = diabetes_time %>%  
+            filter(test_date >= ymd(input$time_filter)) %>%  
+            filter(patient_id %in% recent_data$patient_id)
+          
+        ggplot(temp)+
+            geom_smooth(aes(test_date, a1c_value, group = hba1c_normal), se = F, 
+                        method = "loess")+
+            geom_point(aes(test_date, a1c_value, color = hba1c_normal), alpha = 0.1)+
+            facet_wrap(~hba1c_normal)+
+            theme_bw()+
+            theme(legend.position = "none")
+        }
+        
+        else if(class(diabetes %>% pull(input$diabetes_drop)) == "character") { 
           #categorical variables
           temp = diabetes %>% 
             group_by(across(all_of(c(input$diabetes_drop, input$due_filters)))) %>%  
@@ -72,7 +146,23 @@ server <- function(input, output) {
         }
         
         
-      }
+      } else{ 
+        ggplot(readmit %>%  
+                 filter(last_readmit >= ymd(input$readmit_filter)))+
+          geom_histogram(aes_string(input$readmit_drop), 
+                         fill = "light blue", 
+                         color = "black")+
+          theme_bw()+
+          labs(x = str_to_title(str_replace_all(input$readmit_drop, "_", " ")), 
+               y = "N", 
+               title = paste0(
+                 "Distribution of ", 
+                 str_to_title(str_replace_all(input$readmit_drop, "_", " ")), 
+                 " since ", 
+                 input$readmit_filter
+               )
+               )
+        }
       
        
     })
